1fc8b63b460345b27cc73029b600c4cf0cbe793f
SeanKellyIADT <xseankellyx@gmail.com>
2022-10-21 10:16:42 +0100

    created a index method to get all

--- app/Http/Controllers/carController.php
+++ app/Http/Controllers/carController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;
 
 class CarController extends Controller
 {
@@ -13,7 +14,8 @@ class CarController extends Controller
      */
     public function index()
     {
-        //
+        $cars=Car::all();
+        dd($cars);
     }
 
     /**
