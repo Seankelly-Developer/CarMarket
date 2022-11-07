c8cd5554da372440d1f5f0a35559fd6dfd6b3a89
SeanKellyIADT <xseankellyx@gmail.com>
2022-11-07 13:27:39 +0000

    Added comments

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
+/*The web.php file specifies each route that are to be used for the website's interface */
+
+/*This route is specified in order to return the home screen  */
 Route::get('/', function () {
     return view('/welcome');
 });
+
+
+/*This route is specified in order to return the main screen if a user is logged in */
 Route::resource('/cars', CarController::class)->middleware(['auth']);
+
+
 
 Route::get('/Cars.index', function () {
     return view('Cars.index');
